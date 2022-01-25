90e8a7ecffec36d7f301f6dda7f7b60947e8f0b5
Saeed Nemati <sn.1361@gmail.com>
2022-01-25 10:14:03 +0330

    committed by script

--- DataAccess/CollaborationContext.cs
+++ DataAccess/CollaborationContext.cs
@@ -4,9 +4,9 @@ public class CollaborationContext : DatabaseContext
 {
     public override string ConnectionStringName => "Collaboration";
 
-    public DbSet<Invitation> Invitations { get; set; }
+    public DbSet<Collaboration.Invitation> Invitations { get; set; }
 
-    public DbSet<InvitationView> InvitationViews { get; set; }
+    public DbSet<Collaboration.InvitationView> InvitationViews { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
