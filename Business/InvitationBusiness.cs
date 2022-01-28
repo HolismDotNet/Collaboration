3e619e0d7480f74d035eccc7c91ecf49607b900a
Saeed Nemati <sn.1361@gmail.com>
2022-01-28 14:48:27 +0330

    committed by script

--- Business/InvitationBusiness.cs
+++ Business/InvitationBusiness.cs
@@ -2,9 +2,9 @@ namespace Collaboration;
 
 public class InvitationBusiness : Business<InvitationView, Invitation>
 {
-    protected override Repository<Invitation> WriteRepository => RepositoryInvitation;
+    protected override Repository<Invitation> WriteRepository => Repository.Invitation;
 
-    protected override ReadRepository<InvitationView> ReadRepository => RepositoryInvitationView;
+    protected override ReadRepository<InvitationView> ReadRepository => Repository.InvitationView;
 
     protected override void PreCreation(Invitation invitation)
     {
